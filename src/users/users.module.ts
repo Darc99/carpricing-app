a34237b11cae31412dfc42c21a62c778226757fc
Darc99 <lennyyhugoh@gmail.com>
2024-01-13 23:59:30 +0100

    user entity

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true
+    })
+  ],
   controllers: [UsersController],
   providers: [UsersService]
 })
